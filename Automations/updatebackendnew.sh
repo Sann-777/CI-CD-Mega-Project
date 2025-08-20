12b85b1bd91f2b2ebecee81441bf7af3a3b1ad6b
Sann-777 <contactdevsandeep@gmail.com>
2025-08-20 20:56:21 +0530

    updated automations

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Detect LAN IP (first one on interface)
+# Automatically grab LAN IP (first interface)
 lan_ip=$(hostname -I | awk '{print $1}')
 
 # Path to the .env file
 file_to_find="../backend/.env.docker"
 
-if [ -f $file_to_find ]; then
-    sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${lan_ip}:5173\"|g" $file_to_find
-    echo "Updated FRONTEND_URL to http://${lan_ip}:5173"
+if [ -f "$file_to_find" ]; then
+    # Update FRONTEND_URL with LAN IP
+    sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${lan_ip}:5173\"|g" "$file_to_find"
+    echo "Updated FRONTEND_URL to http://${lan_ip}:5173 in $file_to_find"
 else
-    echo "ERROR: File not found."
+    echo "ERROR: File not found at $file_to_find"
 fi
--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Automatically grab LAN IP
-ipv4_address=$(hostname -I | awk '{print $1}')
+lan_ip=$(hostname -I | awk '{print $1}')
 
+# Path to the .env file
 file_to_find="../frontend/.env.docker"
 current_url=$(cat $file_to_find)
 
-if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
+if [[ "$current_url" != "VITE_API_PATH=\"http://${lan_ip}:31100\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${lan_ip}:31100\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
